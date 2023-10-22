c1cdf47628339c7b1abea948237c1878b8610534
Bharathi Kishna <144966844+Bharathi172@users.noreply.github.com>
2023-10-22 17:32:15 -0400

    Add files via upload

--- GroupProjectEducationPlatform/src/ui/MainJFrame.java
+++ GroupProjectEducationPlatform/src/ui/MainJFrame.java
@@ -89,7 +89,7 @@ public class MainJFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(splitPane, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(splitPane, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 567, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
